7b7b9980ca38c25f67b52c5f97a146015df17dea
Sorkand <96412234+sorkand1@users.noreply.github.com>
2024-11-19 16:20:47 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,27 +1,41 @@
 #!/bin/bash
 
-# Завантаження та виконання common.sh
 source <(curl -s https://raw.githubusercontent.com/R1M-NODES/utils/master/common.sh)
 printLogo
 
-# Перевірка або запит EVM-адреси
 if [ -z "$1" ]; then
-  read -p "Введіть вашу EVM-адресу: " EVM_ADDR
+  read -p "EVM ADDRESS: " EVM_ADDR
 else
   EVM_ADDR=$1
 fi
 
 set -e
 
-sudo apt-get update
-sudo apt-get install -y ca-certificates curl
-sudo install -m 0755 -d /etc/apt/keyrings
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-sudo chmod a+r /etc/apt/keyrings/docker.asc
+if ! command -v docker &> /dev/null; then
+  echo "Docker is not installed. Installing Docker..."
+  sudo apt-get update
+  sudo apt-get install -y ca-certificates curl
+  sudo install -m 0755 -d /etc/apt/keyrings
+  sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+  sudo chmod a+r /etc/apt/keyrings/docker.asc
+  echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  sudo apt-get update
+  sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+else
+  echo "Docker is already installed."
+fi
 
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+if ! docker compose version &> /dev/null; then
+  echo "Docker Compose is not installed. Installing..."
+  sudo apt-get install -y docker-compose-plugin
+else
+  echo "Docker Compose is already installed."
+fi
+
+if [ "$(docker ps -q -f name=verifier)" ]; then
+  echo "Stopping existing container..."
+  sudo docker-compose down
+fi
 
 cat > docker-compose.yml <<EOF
 services:
@@ -30,6 +44,7 @@ services:
     container_name: verifier
     environment:
       - EVM_ADDR=${EVM_ADDR}
+      - CHAIN_ID=534352
     volumes:
       - ./data/data:/app/data
       - ./data/cysic/:/root/.cysic/
